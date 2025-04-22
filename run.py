bbfd90468d9ad0cc2007be3098389268850ce8b6
nvishwak03 <157331939+nvishwak03@users.noreply.github.com>
2025-04-22 01:40:08 -0400

    3d

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ def main():
     # ===============================================
     
     # Input/Output
-    source = 0  # Path to input video file or webcam index (0 for default camera)
+    source = "sample_video.mp4"  # Path to input video file or webcam index (0 for default camera)
     output_path = "output.mp4"  # Path to output video file
     
     # Model settings
