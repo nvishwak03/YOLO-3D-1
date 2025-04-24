bdda2d20a5a5d5551f0d029b8580d8f7d208efcd
nvishwak03 <157331939+nvishwak03@users.noreply.github.com>
2025-04-24 17:30:13 -0400

    Added gdown script to download and extract videos

--- run.sh
+++ run.sh
@@ -2,8 +2,18 @@ python -m venv env
 source env/bin/activate
 pip install -r requirements.txt
 
-echo "Running lane.py..."
+gdown "https://drive.google.com/uc?export=download&id=1F6HAz15xSWQ6oyRgOPy_JP5qgSJvyt7f"
+unzip output_videos.zip
+rm output_videos.zip
+
+gdown "https://drive.google.com/uc?export=download&id=1czq9F8TX8bUkq086IMDL-6H2BUe-GUbn"
+unzip videos.zip
+rm videos.zip
+
+echo "Running lane detection..."
 python lane.py
 
-echo "Running run.py..."
+echo "Running 3D Bounding Box pipeline..."
 python run.py
+
+echo "Process completed successfully!"
